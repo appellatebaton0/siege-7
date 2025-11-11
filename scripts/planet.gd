7f83f444289d1d69f62b0f17ad03a395d9694593
appellatebaton0 <145923930+appellatebaton0@users.noreply.github.com>
2025-11-11 09:36:04 -0600

    v0.3 - Resolution Tweak
    Made the game resolution 1/4th of what it was, to match the UI.

--- scripts/planet.gd
+++ scripts/planet.gd
@@ -5,26 +5,14 @@ class_name Planet extends CharacterBody2D
 
 var in_tractor_beam := false
 
-func dot_product(a:Vector2, b:Vector2) -> float:
-	return (a.x * b.x) + (a.y * b.y)
-
 @onready var lock_point:Vector2 = get_lock_point()
 func get_lock_point() -> Vector2:
 	return $LockPoint.global_position
 
 func move_to_lock_point(delta:float) -> void:
-	var direction = global_position.direction_to(lock_point)
-	var magnitude = global_position.distance_to(lock_point)
-	
-	acceleration = abs(50 - global_position.distance_to(lock_point))
-	
-	var scaler = 3.0 if dot_product(direction, velocity) < 0 else 1.0
-	
-	velocity += direction * magnitude * (20 * delta) * scaler
-	
 	velocity = (lock_point - global_position) * delta * 30
 
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	if not in_tractor_beam:
 		move_to_lock_point(delta)
 		
