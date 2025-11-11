6e3f3e69c6aab326f745484b99dfc187de7b2e83
appellatebaton0 <145923930+appellatebaton0@users.noreply.github.com>
2025-11-11 10:22:59 -0600

    v0.6 - Planet Data
    Added a resource to store the worth and elemental contents of a planet.

---
+++ scripts/planet_data.gd
@@ -0,0 +1,12 @@
+class_name PlanetData extends Resource
+
+@export var name := "Planet"
+
+@export var worth := 0
+
+@export_group("Elements")
+@export var hydrogen := 0
+@export var helium := 0
+@export var oxygen := 0
+@export var carbon := 0
+@export var neon := 0
