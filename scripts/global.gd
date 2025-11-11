bf9b42bf8c99f61758afebbe68ee63ace1020e38
appellatebaton0 <145923930+appellatebaton0@users.noreply.github.com>
2025-11-11 10:52:13 -0600

    v0.8 - Accurate Menu Values
    Made the money/element values on the menu accurately reflect what they should be, and even lerp to what they should be.

---
+++ scripts/global.gd
@@ -0,0 +1,21 @@
+extends Node
+
+@warning_ignore("unused_signal") 
+signal updated_contents
+
+var money := 500
+
+var hydrogen := 0
+var helium := 0
+var oxygen := 0
+var carbon := 0
+var neon := 0
+
+## Changes a value by an amount and updates anything
+## relying on it.
+func change_contents(value:String, by:int):
+	set(value, get(value) + by)
+	updated_contents.emit()
+
+func _ready() -> void:
+	updated_contents.emit()
---
+++ scripts/tray.gd
@@ -0,0 +1,31 @@
+class_name Tray extends Control
+
+@onready var goal_values:Dictionary[String,Dictionary] = {
+	"money": {"goal":0, "value": 0, "node": $Panel/MarginContainer/HBoxContainer/Planets/ElementContainer/PanelContainer2/Money},
+	"hydrogen": {"goal":0, "value": 0, "node": $Panel/MarginContainer/HBoxContainer/Planets/ElementContainer/PanelContainer7/Hydrogen},
+	"helium": {"goal":0, "value": 0, "node": $Panel/MarginContainer/HBoxContainer/Planets/ElementContainer/PanelContainer3/Helium},
+	"oxygen": {"goal":0, "value": 0, "node": $Panel/MarginContainer/HBoxContainer/Planets/ElementContainer/PanelContainer4/Oxygen},
+	"carbon": {"goal":0, "value": 0, "node": $Panel/MarginContainer/HBoxContainer/Planets/ElementContainer/PanelContainer5/Carbon},
+	"neon": {"goal":0, "value": 0, "node": $Panel/MarginContainer/HBoxContainer/Planets/ElementContainer/PanelContainer6/Neon},
+}
+
+func _ready() -> void:
+	Global.updated_contents.connect(_on_updated_contents)
+	_on_updated_contents()
+
+func lerp_content_values() -> void:
+	for value in goal_values.keys():
+		var dict = goal_values[value]
+		
+		dict["value"] = int(move_toward(dict["value"], dict["goal"], 1))
+		
+		dict["node"].text = str(dict["value"]) + " "+ value
+		
+
+func _process(_delta: float) -> void:
+	lerp_content_values()
+	pass
+
+func _on_updated_contents() -> void:
+	for value in goal_values.keys():
+		goal_values[value]["goal"] = Global.get(value)
