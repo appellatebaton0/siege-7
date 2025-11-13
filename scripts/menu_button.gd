a46dbed8e7b0e2a95dd215c49c0ce6b262d7a2c8
appellatebaton0 <145923930+appellatebaton0@users.noreply.github.com>
2025-11-13 12:43:55 -0600

    v0.16 - How To Play
    Made a menu to teach the player how to pay - not as good as a full tutorial, but that'd be a lot of work.

--- scripts/menu_button.gd
+++ scripts/menu_button.gd
@@ -3,15 +3,18 @@ class_name TrayButton extends Button
 var menu_toggle := false
 
 @export var animator:AnimationPlayer
+@export var anim := "open_menu"
+@export var unanim := "close_menu"
 
+@export var s = ["X", "O"]
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("ToggleMenu"):
 		_pressed()
-	text = "X" if menu_toggle else "O"
+	text = s[0] if menu_toggle else s[1]
 
 func _pressed() -> void:
 	if not animator.is_playing():
 		menu_toggle = !menu_toggle
 		
-		animator.play("open_menu" if menu_toggle else "close_menu")
+		animator.play(anim if menu_toggle else unanim)
